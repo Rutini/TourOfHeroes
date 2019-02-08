d6233a314826115006f3708331dbbd79908eb0e3
Vlad Patsuta <vl.patsuta@gmail.com>
2019-02-08 10:27:52 +0200

    Delete string for test

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,12 +3,17 @@ import {Hero} from './hero';
 import {HEROES} from './mock-heroes';
 import {Observable, of} from 'rxjs';
 import {MessageService} from './message.service';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) {
+  }
 
   private heroesUrl = 'api/heroes';  // URL to web api
 
@@ -19,13 +24,6 @@ export class HeroService {
   getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
-  }
-  
-  let str: string = 'Hello git';
-
-  constructor(
-    private http: HttpClient,
-    private messageService: MessageService) {
   }
 
   /** Log a HeroService message with the MessageService */
